19ba2c9c99d2d215ca55813460f4da49f97ffe95
Bart Van Bos <bartvanbos@gmail.com>
2021-03-25 16:10:13 +0100

    added looped script for perf tests

--- performance/scenarios/fortio/connection-test.sh
+++ performance/scenarios/fortio/connection-test.sh
@@ -12,7 +12,7 @@ for res_s in "${RESPONSE_SIZE_ARRAY[@]}" ;do
   echo "========== RESPONSE_SIZE : ${res_s} =========="
   for con in "${CONNECTION_ARRAY[@]}" ; do
     LABELS="onepod-samenode-nosidecar conn-${con} resp-${res_s}"
-    FORTIO_CMD="/usr/bin/fortio load -jitter=true -c=${con} -qps=${QUERIES_PS} -t=${TEST_DURATION} -a -r=0.001 -labels=\"${LABELS}\" http://fortio-server:8080/echo\?size\=${res_s}"
+    FORTIO_CMD="/usr/bin/fortio load -jitter=true -c=${con} -qps=${QUERIES_PS} -t=${TEST_DURATION} -a -r=0.001 -labels='${LABELS}' http://fortio-server:8080/echo\?size\=${res_s}"
     echo "kubectl -n fortio exec -it ${FORTIO_CLIENT} -c fortio -- ${FORTIO_CMD}"
     kubectl -n fortio exec -it ${FORTIO_CLIENT} -c fortio -- ${FORTIO_CMD} | grep "All done"
   done

7453b39583175184966ceb5fd8fe290bee160dc5
Bart Van Bos <bartvanbos@gmail.com>
2021-03-25 15:24:02 +0100

    added looped script for perf tests

--- performance/scenarios/fortio/connection-test.sh
+++ performance/scenarios/fortio/connection-test.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-TEST_DURATION=1m
+FORTIO_CLIENT=$(kubectl get pods -n fortio -l app=fortio-client --output=jsonpath={.items..metadata.name})
 
-CONNECTION_ARRAY=( 16 512 4096 8192 10240 )
-REQUEST_SIZE_ARRAY=( 4 128 512 1024 )
+TEST_DURATION=1m
+QUERIES_PS=0
+
+CONNECTION_ARRAY=( 16 64 512 1024 4096 8192 )
 RESPONSE_SIZE_ARRAY=( 4 128 512 1024 )
 
-for req_s in "${REQUEST_SIZE_ARRAY[@]}" ; do
-  for res_s in "${REQUEST_SIZE_ARRAY[@]}" ;do
-    for con in "${CONNECTION_ARRAY[@]}" ; do
-      echo "REQ_SIZE : ${req_s} , RES_SIZE : ${res_s} , CONN : ${con}"
-      make run RUN_DURATION=${TEST_DURATION} REQUEST_SIZE=${req_s} RESPONSE_SIZE=${res_s} CONNECTIONS=${con} | grep "All done"
-    done
+for res_s in "${REQUEST_SIZE_ARRAY[@]}" ;do
+  for con in "${CONNECTION_ARRAY[@]}" ; do
+
+    echo "RES_SIZE : ${res_s} , CONN : ${con}"
+    FORTIO_CMD="/usr/bin/fortio load \
+      -jitter=true \
+      -c=${con} \
+      -qps=${QUERIES_PS} \
+      -t=${TEST_DURATION} \
+      -a \
+      -r=0.001 \
+      http://fortio-server:8080/echo\?size\=${res_s}"
+    kubectl -n fortio exec -it ${FORTIO_CLIENT} -c fortio -- ${FORTIO_CMD} | grep "All done"
+
   done
 done
